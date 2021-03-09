3836950775c9bc38afe4b819a8685930579f4adc
MDemCode <mdemcode@gmail.com>
2021-03-09 22:11:33 +0100

    w

--- ModelSzkicu/Widok.cs
+++ ModelSzkicu/Widok.cs
@@ -9,6 +9,7 @@ namespace ModelSzkicu {
         public TypWidoku Typ { get; set; }
         //
         public List<LiniaKonturowa> LinieKonturowe { get; set; }
+        // linie wymiarowe czy całe wymiary ???
         public List<Otwor> Otwory { get; set; }
         public List<Fasolka> Fasolki { get; set; }
         public List<Luk> Luki { get; set; }
@@ -16,7 +17,13 @@ namespace ModelSzkicu {
         public Widok(TypWidoku typ) {
             Typ = typ;
             LinieKonturowe = new();
+            Otwory = new();
+            Fasolki = new();
+            Luki = new();
+        }
 
+        public void DodajLinieKonturu(double x1, double x2, double y1, double y2, bool ukryta) {
+            LinieKonturowe.Add(new(x1, x2, y1, y2, ukryta));
         }
 
     }
